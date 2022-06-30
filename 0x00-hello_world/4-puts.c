bf0ffe40ad500481cdc2e3dcca1ab28e34657a68
Racheal mwikali <kasiaracheal@gmail.com>
2022-06-30 10:43:28 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,10 @@
-#!/bin/bash
 #include <stdio.h>
-
-
 
 /**
 
- * main - Entry point
+ * main - prints the phrase "Programming is like building a multilingual puzzle
 
- *
-
- * Return: Always 0 (Success)
+ * Return: 0 if exited properly, non-zero otherwise
 
  */
 
@@ -17,8 +12,8 @@ int main(void)
 
 {
 
-	puts("\"Programming is like building a multilingual puzzle");
+ puts("\"Programming is like building a multilingual puzzle");
 
-	return (0);
+ return (0);
 
 }
